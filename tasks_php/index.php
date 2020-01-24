f268738fe55bd85e2b020549cee8c2e9272a6129
Virga <virginia.andersson@ufpel.edu.br>
2020-01-24 09:28:03 -0300

    http in index.php

--- tasks_php/index.php
+++ tasks_php/index.php
@@ -1,5 +1,40 @@
-document.querySelectorAll( ".editdata" ).forEach( element => {
-	element.addEventListener( "blur", event => {
-		console.log( element.innerText, element.id, element.dataset.id );
-	});
-});
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Tarefas</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+
+        <div class="header">
+            <p>
+                <?php
+                    echo "Tarefas";
+                ?>
+            </p>
+        </div>
+
+        <div class="content">
+            <table>
+                <!-- <thead>
+                    <tr>
+                        <th>Concluído</th>
+                        <th>Tarefa</th>
+                    </tr>
+                </thead> -->
+                <tbody>
+                    <tr>
+                        <td class="checkbox">
+                            <input type="checkbox" name="selectDone" value="unchecked" />
+                        </td>
+                        <td id="data1" data-id="algum conteudo" contenteditable="true" class="editdata">
+                            Nova Tarefa
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    <script src="index.js"></script>
+    </body>
+</html>
