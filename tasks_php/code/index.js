2cc2da106f88d89b90e4f66187d25af71cf48b6f
Virga <virginia.andersson@ufpel.edu.br>
2020-01-31 11:27:40 -0300

    Old Code directory removed

--- tasks_php/code/index.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll( ".editdata" ).forEach( element => {
-	element.addEventListener( "blur", event => {
-		console.log( element.innerText, element.id, element.dataset.id );
-	});
-});
